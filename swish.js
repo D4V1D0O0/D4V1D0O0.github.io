c3a8e6465b44b5d2954a9fa0e6c03b302f9b4a0a
David Bergvall <davidbergvall04@gmail.com>
2026-01-17 13:01:22 +0100

    test open swish app

---
+++ swish.js
@@ -0,0 +1,7 @@
+
+window.location = "swish://paymentrequest?token=c28a4061470f4af48973bd2a4642b4fa&callbackurl=merchant%253A%252F%252F";
+let openSwishBtn = document.getElementById("openSwish");
+
+openSwishBtn.onclick = function() {
+    window.location = "swish://paymentrequest?token=c28a4061470f4af48973bd2a4642b4fa&callbackurl=merchant%253A%252F%252F";
+}
