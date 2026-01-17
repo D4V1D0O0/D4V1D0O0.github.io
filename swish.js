3819b4531f0704f35ad4209fe5b9d4276ef1f82e
David Bergvall <davidbergvall04@gmail.com>
2026-01-17 13:16:56 +0100

    test

--- swish.js
+++ swish.js
@@ -2,5 +2,5 @@
 let openSwishBtn = document.getElementById("openSwish");
 
 openSwishBtn.onclick = function() {
-    window.location = "swish://paymentrequest?token=c28a4061470f4af48973bd2a4642b4fa&callbackurl=d4v1d0o0.github.io";
+    window.location = "swish://paymentrequest?receiver=0701234567&amount=125&message=Test";
 }
